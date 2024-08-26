0c96573d632680d4f069a64f99af11a7138232e5
Jordan Pierce <jordan.pierce@noaa.gov>
2024-08-26 16:19:38 -0400

    install script version

--- install.py
+++ install.py
@@ -31,8 +31,8 @@ python_v = f"{sys.version_info[0]}{sys.version_info[1]}"
 python_sub_v = int(sys.version_info[1])
 
 # check python version
-if python_sub_v != 9:
-    raise Exception(f"Only Python 3.9 is supported.")
+if python_sub_v != 8 or python_sub_v != 9:
+    raise Exception(f"Only Python 3.8 and 3.9 are supported.")
 
 # ---------------------------------------------
 # Requirements file
