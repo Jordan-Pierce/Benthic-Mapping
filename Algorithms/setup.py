77c299f739dffc3ab39bd754ff36576124147ef4
Jordan Pierce <115024024+Jordan-Pierce@users.noreply.github.com>
2024-09-13 10:07:39 -0400

    Update setup.py

--- Algorithms/setup.py
+++ Algorithms/setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        'msvc-runtime',
+        # 'msvc-runtime',
         'wheel',
         'dill',
         'tqdm',
